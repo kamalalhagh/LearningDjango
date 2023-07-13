5dee4fe57c478fe29e8c4167bc835a4adb09bc5e
Kamalalhagh <kamalalhagh@gmail.com>
2023-07-13 15:28:31 +1000

    Add a template webpage

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('webpage', include("webpage.urls"))
+    path('', include("webpage.urls"))
 ]
--- mysite/webpage/views.py
+++ mysite/webpage/views.py
@@ -1,6 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the webpage index.")
+    return render(request, "webpage/index.html")
 
+# from django.http import HttpResponse
+#
+#
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the webpage index.")
